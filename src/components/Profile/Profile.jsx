02a08bf1848357cc29021409eb1065cf5d8db61d
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-09-05 18:00:43 +0300

    1+css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { styled } from 'styled-components';
 
 export const Profile = ({
   username,
@@ -8,29 +9,29 @@ export const Profile = ({
   stats: { followers, views, likes },
 }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}f</p>
-        <p className="tag">{`@${tag}`}</p>
-        <p className="location">{location}</p>
-      </div>
+    <ProfileWrap>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <AvatarName>{username}f</AvatarName>
+        <AvatarTag>{`@${tag}`}</AvatarTag>
+        <AvatarLocation>{location}</AvatarLocation>
+      </Description>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
+      <ProfStats>
+        <ProfStatsItem>
+          <StatsLabel>Followers</StatsLabel>
+          <StatsQuantity>{followers}</StatsQuantity>
+        </ProfStatsItem>
+        <ProfStatsItem>
+          <StatsLabel>Views</StatsLabel>
+          <StatsQuantity>{views}</StatsQuantity>
+        </ProfStatsItem>
+        <ProfStatsItem>
+          <StatsLabel>Likes</StatsLabel>
+          <StatsQuantity>{likes}</StatsQuantity>
+        </ProfStatsItem>
+      </ProfStats>
+    </ProfileWrap>
   );
 };
 
@@ -45,3 +46,74 @@ Profile.propTypes = {
     likes: PropTypes.number,
   }),
 };
+
+const ProfileWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: auto;
+  margin-top: 50px;
+  width: min-content;
+`;
+
+const Description = styled.div`
+  /* position: relative; */
+  text-align: center;
+
+  width: 100%;
+  padding: 16px;
+
+  border-radius: 28px;
+  background: #ffffff;
+  /* border: 10px, solid, blue; */
+  box-shadow: 10px 0px 10px rgba(89, 213, 189, 0.45);
+`;
+
+const Avatar = styled.img`
+  width: 300px;
+`;
+const AvatarName = styled.p`
+  margin-left: 30px;
+  color: #161414b5;
+  font-size: 30px;
+  font-weight: 700;
+`;
+const AvatarTag = styled.p`
+  color: #161414b5;
+  font-size: 20px;
+  font-weight: 400;
+`;
+const AvatarLocation = styled.p`
+  color: #161414b5;
+  font-size: 20px;
+  font-weight: 400;
+`;
+export const ProfStats = styled.ul`
+  list-style: none;
+  display: flex;
+  padding: 12px;
+  margin: 0;
+`;
+const ProfStatsItem = styled.li`
+  display: flex;
+  width: 120px;
+  justify-items: center;
+  :not(:last-child) {
+    border-right: 1px solid #999999;
+  }
+`;
+
+export const StatsLabel = styled.span`
+  font-weight: 400;
+  font-size: 12px;
+
+  margin-bottom: 10px;
+  color: #999999;
+`;
+export const StatsQuantity = styled.span`
+  font-weight: 700;
+  font-size: 24px;
+
+  color: #000000;
+`;
